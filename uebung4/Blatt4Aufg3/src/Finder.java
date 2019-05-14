db85555c45a011790929b009b587095c028c365b
Bettina E <bettina.esser@zembra.de>
2019-05-14 12:34:32 +0200

    "Index out of range gefixt"

--- uebung4/Blatt4Aufg3/src/Finder.java
+++ uebung4/Blatt4Aufg3/src/Finder.java
@@ -3,7 +3,7 @@ public class Finder {
 	public static Comparable findMin(Comparable[] x) {
 		Person kleinstes = (Person)x[0];
 		int n = x.length;
-		for(int i = 1; i <= n; i++) {
+		for(int i = 1; i < n; i++) {
 			if(kleinstes.compareTo(x[i]) > 0) {
 				kleinstes = (Person)x[i];
 			}
